8604d82452536e5135229798a1f8109d06f47b1b
Sidhant4912 <siddhantpawar64@gmail.com>
2026-04-06 19:42:49 +0530

    updated barcode section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import QRScanner from "./QRScanner";
 import Menu from "./Menu";
-import './app.css'
+import './App.css'
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Menu />} />
-        <Route path="/menu" element={<QRScanner></QRScanner>} />
+        <Route path="/barcode" element={<QRScanner></QRScanner>} />
       </Routes>
     </BrowserRouter>
   );
